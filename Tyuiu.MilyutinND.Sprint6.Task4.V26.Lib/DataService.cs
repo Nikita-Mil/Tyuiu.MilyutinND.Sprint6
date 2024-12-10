23081580274c4782896ff1be40ba2b557e45303c
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-12-10 23:16:17 +0500

    4

--- Tyuiu.MilyutinND.Sprint6.Task4.V26.Lib/DataService.cs
+++ Tyuiu.MilyutinND.Sprint6.Task4.V26.Lib/DataService.cs
@@ -26,7 +26,7 @@ namespace Tyuiu.MilyutinND.Sprint6.Task4.V26.Lib
                     count++;
                 }
             }
-            return [19.81, 16.79, 13.87, 10.98, 7.94, 3.0, 3.23, -0.45, -3.79, -6.97, -10.0];
+            return valueArray;
         }
     }
 }
