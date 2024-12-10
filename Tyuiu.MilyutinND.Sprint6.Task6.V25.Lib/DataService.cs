7b0a423320285a26782c07c69e54088f3b6df8aa
Tyuiu.milyutin@outlook.com <mm1llnikita@gmail.com>
2024-12-11 00:14:22 +0500

    6

--- Tyuiu.MilyutinND.Sprint6.Task6.V25.Lib/DataService.cs
+++ Tyuiu.MilyutinND.Sprint6.Task6.V25.Lib/DataService.cs
@@ -8,37 +8,7 @@ namespace Tyuiu.MilyutinND.Sprint6.Task6.V25.Lib
     {
         public string CollectTextFromFile(string str, string path)
         {
-            string resStr = "";
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    if (line == "")
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        List<string> result = new List<string>();
-                        MatchCollection words = Regex.Matches(line, @"[А-Яа-яA-Za-z]+");
-                        foreach (var word in words)
-                        {
-                            result.Add(word.ToString());
-                        }
-
-                        foreach (var word in result)
-                        {
-                            if (word.Contains("E") || word.Contains("e"))
-                            {
-                                resStr += word + " ";
-                            }
-                        }
-                    }
-                }
-            }
-            resStr = resStr.Trim();
-            return "ISprint6Task6V25";
+            
         }
     }
 }
--- Tyuiu.MilyutinND.Sprint6.Task6.V25/FormMain.cs
+++ Tyuiu.MilyutinND.Sprint6.Task6.V25/FormMain.cs
@@ -9,27 +9,9 @@ namespace Tyuiu.MilyutinND.Sprint6.Task6.V25
             InitializeComponent();
         }
 
-        string openFilePath;
-        DataService ds = new DataService();
-        private void buttonDone_Click(object sender, EventArgs e)
+        private void InitializeComponent()
         {
-            string str = "**";
-            groupBoxOutPutData.Text = ds.CollectTextFromFile(str, openFilePath);
-        }
 
-        private void buttonHelp_Click(object sender, EventArgs e)
-        {
-            FormAbout formAbout = new FormAbout();
-            formAbout.ShowDialog();
-        }
-
-        private void buttonOpenFile_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-            openFilePath = openFileDialog1.FileName;
-            textBoxLoadFromFile.Text = File.ReadAllText(openFilePath);
-            groupBoxOutPutData.Text = groupBoxOutPutData.Text + " " + openFileDialog1.FileName;
-            buttonDone.Enabled = true;
         }
     }
 }
